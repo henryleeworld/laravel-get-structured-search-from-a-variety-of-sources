ffa50cd3ef745fd79cec767ef30a70d77c03c077
Henry Lee <henryleeworld@gmail.com>
2024-05-16 00:08:10 +0800

    feat:更新最新資料

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,6 +55,7 @@ class User extends Authenticatable implements Searchable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
 
     public function posts()
